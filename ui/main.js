2fc166fe7b595f5ace7a42a0a52fa0e6f031e600
shankarvikraman <31060258+shankarvikraman@users.noreply.github.com>
2017-08-21 11:27:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+//to move the madi
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft =marginLeft +"px";
+    
+};
+img.onload = function(){
+    var interval=setinterval(moveRight,10);
+};
