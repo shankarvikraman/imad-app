9c3493d2d05b112687301b3880e89ea0347a5851
shankarvikraman <31060258+shankarvikraman@users.noreply.github.com>
2017-08-21 11:41:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 1;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft =marginLeft +"px";
      if (marginLeft == 500)
        marginLeft = 0;
