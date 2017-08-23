daa0d4d03b4ae5c8d75fc1f51de856420e99d755
shankarvikraman <31060258+shankarvikraman@users.noreply.github.com>
2017-08-23 19:10:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.get('/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res){
+    counter = counter +1;
+    res.send(counter,tostring());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
